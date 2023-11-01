db6089ac49c95a9687c5218735fd50cecb6dbef5
knpoblete <pobletekath@gmail.com>
2023-11-01 17:57:14 +0000

    fixed func

--- Home.py
+++ Home.py
@@ -1,10 +1,10 @@
 import streamlit as st
-from utils import set_font
+from utils import set_font, set_text
 from PIL import Image
 
 
 st.set_page_config(layout="wide")
-st.markdown(set_font('30-day Map Challenge','h1'),unsafe_allow_html=True)
+st.markdown(set_text('30-day Map Challenge','h1'),unsafe_allow_html=True)
 
 st.markdown(set_font('Hello, I\'m Kath!','h2'),unsafe_allow_html=True)
 st.write('I will be participating in the 30-day Map Challenge. Join me as I learn to make maps!')
