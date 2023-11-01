3813da437880ae3c4533f28671213bde3db0887e
knpoblete <pobletekath@gmail.com>
2023-11-01 17:57:36 +0000

    fixed func

--- Home.py
+++ Home.py
@@ -6,7 +6,7 @@ from PIL import Image
 st.set_page_config(layout="wide")
 st.markdown(set_text('30-day Map Challenge','h1'),unsafe_allow_html=True)
 
-st.markdown(set_font('Hello, I\'m Kath!','h2'),unsafe_allow_html=True)
+st.markdown(set_text('Hello, I\'m Kath!','h2'),unsafe_allow_html=True)
 st.write('I will be participating in the 30-day Map Challenge. Join me as I learn to make maps!')
 
 st.write("Check out the challenge here: [https://30daymapchallenge.com/](https://30daymapchallenge.com/)")
